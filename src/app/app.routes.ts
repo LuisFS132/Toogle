1d0438881808c75d80d50d707922b1d3916439b0
LuisFS132 <jluisfs132@gmail.com>
2025-03-19 08:51:05 -0600

    creado likes component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import { InicioToComponent } from './componentes/inicio-to/inicio-to.component';
+import { LikesComponent } from './componentes/likes/likes.component';
 
 export const routes: Routes = [
 
     {
         path: 'inicio',
         component : InicioToComponent
+    },
+    {
+        path: 'likes',
+        component : LikesComponent
     },
     {
         path: '',
