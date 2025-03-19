986bad72a68aec38dd6c27b18e3b9206b009e5fa
LuisFS132 <jluisfs132@gmail.com>
2025-03-19 08:58:45 -0600

    Creacion de componentes y cambio de archivo de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { InicioToComponent } from './componentes/inicio-to/inicio-to.component';
 import { LikesComponent } from './componentes/likes/likes.component';
+import { ComentariosComponent } from './componentes/comentarios/comentarios.component';
+import { RolesComponent } from './componentes/roles/roles.component';
+import { UsuariosComponent } from './componentes/usuarios/usuarios.component';
 
 export const routes: Routes = [
 
@@ -11,6 +14,18 @@ export const routes: Routes = [
     {
         path: 'likes',
         component : LikesComponent
+    },
+    {
+        path: 'comentarios',
+        component : ComentariosComponent
+    },
+    {
+        path: 'roles',
+        component : RolesComponent
+    },
+    {
+        path: 'usuarios',
+        component : UsuariosComponent
     },
     {
         path: '',
