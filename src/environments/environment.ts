a624db1ea9973b4346b321bac02bac3650d16f1b
Joslavg <lemlen08op@gmail.com>
2025-04-07 08:15:03 -0600

    asdasd
    asdasda

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+    apiKey: "TU_API_KEY",
+    authDomain: "TU_DOMINIO.firebaseapp.com",
+    projectId: "TU_PROJECT_ID",
+    storageBucket: "TU_BUCKET.appspot.com",
+    messagingSenderId: "SENDER_ID",
+    appId: "APP_ID"
+    }
+};
