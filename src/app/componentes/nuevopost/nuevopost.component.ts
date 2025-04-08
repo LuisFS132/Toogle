f6422053999b91c56b8a1f409831c668b6f6693c
LuisFS132 <jluisfs132@gmail.com>
2025-04-08 11:55:48 -0600

    publicar

--- src/app/componentes/nuevopost/nuevopost.component.ts
+++ src/app/componentes/nuevopost/nuevopost.component.ts
@@ -4,10 +4,11 @@ import { InicioToService } from '../../servicios/inicio-to.service';
 import { FormsModule } from '@angular/forms';
 import { JsonPipe } from '@angular/common';
 import { AngularFireAuth } from "@angular/fire/compat/auth";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nuevopost',
-  imports: [FormsModule, JsonPipe],
+  imports: [FormsModule, JsonPipe, CommonModule],
   templateUrl: './nuevopost.component.html',
   styleUrl: './nuevopost.component.css'
 })
