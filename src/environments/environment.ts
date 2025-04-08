367ebae127fccbfb2c1644b4599c27cea66ec911
Joslavg <lemlen08op@gmail.com>
2025-04-07 19:10:58 -0600

    dasdas
    dasdada

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,13 @@
+
+
 export const environment = {
     production: false,
     firebaseConfig: {
-    apiKey: "TU_API_KEY",
-    authDomain: "TU_DOMINIO.firebaseapp.com",
-    projectId: "TU_PROJECT_ID",
-    storageBucket: "TU_BUCKET.appspot.com",
-    messagingSenderId: "SENDER_ID",
-    appId: "APP_ID"
+    apiKey: "AIzaSyBHCEN4YaWgp57R7lwjZkPoK_vQ43pAi-Q",
+    authDomain: "toogle-e1d34.firebaseapp.com",
+    projectId: "toogle-e1d34",
+    storageBucket: "toogle-e1d34.firebasestorage.app",
+    messagingSenderId: "929192060038",
+    appId: "1:929192060038:web:5a57f28d6048ad64831d07"
     }
 };
